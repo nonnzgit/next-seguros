484484e8add19ad3db8f867492b86326a726ffcd
Juan <jdvelrod@gmail.com>
2021-12-26 15:35:32 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      Esta cargando el index que no debería cargar del proyecto Seguros.js
+    </div>
+  );
+}
